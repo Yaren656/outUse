8084dea8ddbf7c9f2cb02702f89639992b9439a7
Yaren Yuvka <yarenyuvka@gmail.com>
2021-01-17 22:15:02 +0300

    Add project files.

---
+++ outUse/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace outUse
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number1; //değer atayabiliriz fakat out'da gerek yok.
+            int number2 = 3;
+            var result = Multiply(out number1, number2);
+
+            Console.WriteLine("Sonuç : " + result);
+        }
+        
+        static int  Multiply (out int number1, int number2)
+        {
+            number1 = 20; //out'da bunu eklemek zorundayız.
+            return number1 * number2;
+        }
+    }
+}
